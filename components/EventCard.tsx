c96dc4de4a4e979d482d105a68209e1f6aa57aa6
San <san@Sans-MacBook-Pro.local>
2026-09-11 16:11:58 +0700

    implement-posthog

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import posthog from "posthog-js";
 
 interface Props{
     title : string;
@@ -12,7 +15,15 @@ interface Props{
 
 const EventCard = ({title,image,slug,location,date,time}:Props) => {
   return (
-    <Link href={`events/${slug}`} id="events-card">
+    <Link
+      href={`events/${slug}`}
+      id="events-card"
+      onClick={() => {
+        if (process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+          posthog.capture("event_selected", { event_slug: slug });
+        }
+      }}
+    >
         <Image src={image} alt={title} width={410} height={300} className="poster"/>
         
         <div className="flex flex-row gap-2">
