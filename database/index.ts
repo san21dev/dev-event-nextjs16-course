b89855b107ad05c5613d43ef5cbcdfa28b3547ae
San <san@Sans-MacBook-Pro.local>
2026-09-12 09:34:52 +0700

    implement database model

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+export { default as Booking } from "./booking.model";
+export { default as Event } from "./event.model";
+
+export type { IBooking as BookingDocument } from "./booking.model";
+export type { IEvent as EventDocument } from "./event.model";
